ad8457eee0d86e2457378dc1835a3e4248418812
YanivAvraham <yanivemail5@gmail.com>
2024-05-11 12:31:37 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/reactrouter/",
+  server: {
+    open: true,
+  },
+  // Added the following line
+  navigateFallbackRoot: "/reactrouter/index.html",
 });
